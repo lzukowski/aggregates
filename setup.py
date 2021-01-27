7aa64256dde38cc9d3a8c9bf42cb7d7fec7ffab6
lzukowski <lukasz.zukowski@gmail.com>
2021-01-27 21:04:15 +0100

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='aggregates',
+    version='0.0.0',
+    install_requires=['eventsourcing[sqlalchemy]'],
+    tests_require=['pytest'],
+    package_dir={'': 'src'},
+    packages=['project_management'],
+    py_modules=[],
+)
